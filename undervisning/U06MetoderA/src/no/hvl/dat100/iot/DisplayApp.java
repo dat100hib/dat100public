32ed7c91deea4d86deb23664fdeaee2116e428a7
Lars Kristensen <lmkr@hib.no>
2020-08-30 14:02:55 +0200

    Kodeformattering sensor eksempel

--- undervisning/U06MetoderA/src/no/hvl/dat100/iot/DisplayApp.java
+++ undervisning/U06MetoderA/src/no/hvl/dat100/iot/DisplayApp.java
@@ -31,14 +31,15 @@ public class DisplayApp {
 
 	}
 
-	private static String toString (double x) {
+	private static String twoDigits (double x) {
 		return Double.toString(Math.round(x*100) / 100.0);
 	}
 	
 	private static void display(int i, double current, double min, double max, double sum) {
 
-		System.out.println(i + ":" + toString(current) + "[" + toString(min) + " " + toString(max) + "]" + toString(sum/i));
-
+		System.out.println(i + ":" + twoDigits(current) + "[" 
+							 + twoDigits(min) +  " " + twoDigits(max) 
+							 + "]" + twoDigits(sum/i));
 	}
 
 	private static void delay() {
