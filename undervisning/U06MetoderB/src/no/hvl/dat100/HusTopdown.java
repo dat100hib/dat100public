5258edfd91f6a3bd247a3381baad5f8ffb1d78be
Lars Kristensen <lmkr@hib.no>
2020-09-04 20:10:32 +0200

    Topdown - fjernet kode fra mal

--- undervisning/U06MetoderB/src/no/hvl/dat100/HusTopdown.java
+++ undervisning/U06MetoderB/src/no/hvl/dat100/HusTopdown.java
@@ -11,36 +11,6 @@ public class HusTopdown extends EasyGraphics {
 	public void run() {
 		makeWindow("Husrekke");
 
-		// tegnHusRekke(5,20,200,60,40,10);
-		
-		tegnTrekant(20,200,60,40);
 	}
 
-	public void tegnHusRekke(
-			int antallHus,
-			int startX, int startY,
-			int bredde, int hoyde, int avstand) {
-		
-		for (int husNr = 1; husNr <= antallHus; husNr++) {
-		
-			tegnHus(startX,startY,bredde,hoyde);
-			startX = startX + bredde + avstand;
-		}
-	}
-	
-	public void tegnHus(int x, int y, int bredde, int hoyde) {
-		
-		int veggTopp = y - hoyde / 2;
-		
-		drawRectangle(x,veggTopp,bredde,hoyde/2);
-		tegnTrekant(x,veggTopp,bredde,hoyde/2);
-	}
-	
-	public void tegnTrekant(int x, int y, int bredde, int hoyde) {
-		
-		drawLine(x,y,x+bredde,y);
-		drawLine(x+bredde,y,x+bredde/2,y-hoyde);
-		drawLine(x+bredde/2,y-hoyde,x,y);
-		
-	}
 }
