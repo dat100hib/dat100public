83630f55f8438ad51eee056c8d458d7531ce1ef8
Lars Michael Kristensen <lmkr@hvl.no>
2020-09-07 11:34:52 +0200

    Added quiz-kode

---
+++ undervisning/U07Tabeller/src/no/hvl/dat100/eksempel/Quiz.java
@@ -0,0 +1,42 @@
+package no.hvl.dat100.eksempel;
+
+public class Quiz {
+
+	public static void main(String[] args) {
+		
+		// hvilke oppretter en tabell
+
+		double[] tab1;
+		
+		boolean[] tab2 = {true,false,true};
+
+		int[] tab3 = new int[4];
+		
+		// hvilke deklarere en tabell variabel
+		double[] tab4;
+		
+		int[] tab5 = new int[4];
+
+		tab3 = tab5;
+		
+		// hva er første indeks i en tabell
+		
+		// hva er siste indeks i en tabell med lengde 4
+		
+		// kan tabellvariable opprettes dynamisk under programkjørsel
+		
+		// kan lengden på en tabell endres dynamisk under programkjørsel
+		
+		// Hva blir overført når en tabell er parameter i en metode?
+		// alle verdien i tabellen
+		// en referanse til tabellen
+		
+		double[] tab = {5.6,7.8,8.9,10};
+		
+		System.out.println(tab[1]);
+		
+		tab[3] = 7;
+		
+	}
+
+}
