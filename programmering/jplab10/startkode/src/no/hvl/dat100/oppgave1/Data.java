4f51822e23766f4f7546e7ff89f1e23f0693a866
Lars Kristensen <lmkr@hib.no>
2020-10-14 16:17:46 +0200

    Data tabell eksempel

---
+++ programmering/jplab10/startkode/src/no/hvl/dat100/oppgave1/Data.java
@@ -0,0 +1,20 @@
+package no.hvl.dat100.oppgave1;
+
+public class Data {
+
+	private int data;
+
+	public Data(int d) {
+		data = d;
+	}
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+	public String toString() {
+		return "" + data;
+	}
+}
+
