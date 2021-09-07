5fd2c2893ea4e9f6596a41d13a97a0a707654e98
Lars Kristensen <larskristensen@158-37-70-229.hvl.no>
2021-09-07 10:59:32 +0200

    U06 verdi eksempel

---
+++ undervisning/U06MetoderB/src/no/hvl/dat100/VerdiOverforing.java
@@ -0,0 +1,19 @@
+package no.hvl.dat100;
+
+public class VerdiOverforing {
+	
+	public static void main(String[] args) {
+
+		int x = 1;
+		
+		increment(x);
+		
+		System.out.println(x);
+	}
+	
+	public static void increment(int y) {
+		
+		y = y + 1;
+		
+	}
+}
