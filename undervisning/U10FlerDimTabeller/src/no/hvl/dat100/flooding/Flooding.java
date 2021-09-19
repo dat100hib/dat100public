05d1aa2d63f5ad082c9f43c52905eafb801d9161
Lars Kristensen <lmkr@hib.no>
2021-09-19 10:34:20 +0200

    indent flooding

--- undervisning/U10FlerDimTabeller/src/no/hvl/dat100/flooding/Flooding.java
+++ undervisning/U10FlerDimTabeller/src/no/hvl/dat100/flooding/Flooding.java
@@ -10,7 +10,9 @@ public class Flooding extends EasyGraphics {
 	int MARGIN = 50;
 
 	// terreng hogde
-	int[][] terreng = { { 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 }, { 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 },
+	int[][] terreng = { 
+			{ 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 }, 
+			{ 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 },
 			{ 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 } };
 
 	public static void main(String[] args) {
