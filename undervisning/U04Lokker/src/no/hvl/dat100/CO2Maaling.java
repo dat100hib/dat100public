933386fff7ba29cfe8c4853675e0acb4670c69dd
Lars Kristensen <larskristensen@158-37-70-25.hvl.no>
2022-08-24 09:39:06 +0200

    U04 start

---
+++ undervisning/U04Lokker/src/no/hvl/dat100/CO2Maaling.java
@@ -0,0 +1,10 @@
+package no.hvl.dat100;
+
+public class CO2Maaling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ undervisning/U04Lokker/src/no/hvl/dat100/LoginEksempel.java
@@ -0,0 +1,10 @@
+package no.hvl.dat100;
+
+public class LoginEksempel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
