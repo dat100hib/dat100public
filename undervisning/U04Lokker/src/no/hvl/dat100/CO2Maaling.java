cea5b4fce156a2232f64b1ba8d3bce6d67b9a804
Lars Kristensen <larskristensen@158-37-70-25.hvl.no>
2022-08-26 16:08:16 +0200

    U04 eksempler

--- undervisning/U04Lokker/src/no/hvl/dat100/CO2Maaling.java
+++ undervisning/U04Lokker/src/no/hvl/dat100/CO2Maaling.java
@@ -1,10 +1,34 @@
 package no.hvl.dat100;
 
-public class CO2Maaling {
+import static java.lang.Integer.parseInt;
+import easygraphics.*;
 
+public class CO2Maaling extends EasyGraphics {
+	
+	private static int ANTALL = 10;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    launch(args);
+	  }
 
-	}
+	  public void run()  {
+		  
+	    makeWindow("CO-2 nivå indikator");
+
+	    for (int i = 1; i<ANTALL; i++) {
+	    	
+	    	int co2 = parseInt( getText("PPM: [" + i + "]" ));
+			
+	 	    if (co2 < 500) {
+	 	    	setColor(0, 255, 0); // green
+	 	    } else {
+	 	    	setColor(255,0,0); // red
+	 	    }
+	 	    
+	 	    fillCircle(100, 160, 60);
+	 	    
+	 	   
+	    }
+	  }
 
 }
--- undervisning/U04Lokker/src/no/hvl/dat100/LoginEksempel.java
+++ undervisning/U04Lokker/src/no/hvl/dat100/LoginEksempel.java
@@ -1,10 +1,73 @@
 package no.hvl.dat100;
 
+import java.util.Scanner;
+
 public class LoginEksempel {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String brukernavn = "lars";
+		String passord = "hemli";
+
+		Scanner input = new Scanner(System.in);
+
+		String funksjonTxt = "Velg funksjon\n a) - opprette konto\n b) - endre passord\n c) - slette konto\n d) - login";
+
+		System.out.println(funksjonTxt);
+		System.out.print(">");
+
+		String valgTxt = input.nextLine();
+
+		switch (valgTxt) {
+
+		case "a":
+			System.out.print("Opprett konto\nBrukernavn:");
+			brukernavn = input.nextLine();
+			System.out.print("Passord:");
+			passord = input.nextLine();
+			break;
+		case "b":
+			System.out.print("Endre passord\nGammelt passord:");
+			passord = input.nextLine();
+			System.out.print("Nytt passord:");
+			passord = input.nextLine();
+			break;
+		case "c":
+			System.out.print("Slette bruker\nBrukernavn:");
+			brukernavn = input.nextLine();
+			System.out.print("Passord:");
+			passord = input.nextLine();
+			break;
+		case "d":
+			System.out.print("Login\nBrukernavn:");
+			brukernavn = input.nextLine();
+			
+			boolean loggedinn = false;
+			int forsok = 0;
+			
+			do {
+				
+				forsok++;
+				
+				System.out.print("Passord:");
+				String inpassord = input.nextLine();
+				
+				if (inpassord.equals(passord)) {
+					System.out.println("Logged inn");
+					loggedinn = true;
+				} else {
+					System.out.println("Feil i passord");
+				}
+				
+			} while (!loggedinn && forsok <= 3);
+			break;
+		default:
+			System.out.println("Ingen funksjon valgt");
+		}
+
+		System.out.println(brukernavn);
+		System.out.println(passord);
+
+		input.close();
 	}
-
 }
