caf4d179b5736dff7ad7c34448b53fb1aed60fb4
Lars Kristensen <larskristensen@158-37-71-196.hvl.no>
2022-09-08 10:25:42 +0200

    InnleseTall update

--- undervisning/U07Tabeller/src/no/hvl/dat100/eksempel/InnleseTall.java
+++ undervisning/U07Tabeller/src/no/hvl/dat100/eksempel/InnleseTall.java
@@ -4,19 +4,25 @@ import static java.lang.Integer.parseInt;
 import static java.lang.Double.parseDouble;
 import static javax.swing.JOptionPane.showInputDialog;
 
+import java.util.Scanner;
+
 public class InnleseTall {
 
+	private static Scanner input;
+	
 	public static void main(String[] args) {
 			 
+		input = new Scanner(System.in);
+		
 		// TODO - START
 		
-	    int antall = lesInteger("Hvor mange tall skal leses inn?");
+	    int antall = lesInteger("Antall tall som skal leses inn: ");
 	    
 	    double[] tall = new double[antall];
 	    
 	    for (int i = 0;i<tall.length; i++) {
 	    	
-	    	double verdi = lesDouble("Tall: " + i);
+	    	double verdi = lesDouble("Tall " + i + ": ");
 	    	
 	    	tall[i] = verdi;
 	    	
@@ -26,11 +32,15 @@ public class InnleseTall {
 	    
 	    // TODO - END
 	    
+	    input.close();
+	    
 	}
 	
 	private static double lesDouble(String message) {
 		
-		String tallTxt = showInputDialog(message);
+		System.out.print(message);
+		
+		String tallTxt = input.nextLine();
 	    
 	    double tall = parseDouble(tallTxt);
 	    
@@ -40,12 +50,13 @@ public class InnleseTall {
 	
 	private static int lesInteger(String message) {
 		
-		String tallTxt = showInputDialog(message);
-	    
+		System.out.print(message);
+		
+		String tallTxt = input.nextLine();
+		    
 	    int tall = parseInt(tallTxt);
 	    
 	    return tall;
-	    
 	}
 	
 	// TODO - START
