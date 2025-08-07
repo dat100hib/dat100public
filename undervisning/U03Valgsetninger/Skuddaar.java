91951dff5758e3965ea09fe494ca9c0b52844ed4
Lars Michael Kristensen <lmkr@users.noreply.github.com>
2025-08-07 19:14:29 +0200

    U03 eksempler review

--- undervisning/U03Valgsetninger/Skuddaar.java
+++ undervisning/U03Valgsetninger/Skuddaar.java
@@ -1,42 +1,36 @@
-package no.hvl.dat100;
-
-import static java.lang.Integer.*;
-import static javax.swing.JOptionPane.showInputDialog;
-import static javax.swing.JOptionPane.showMessageDialog;
-
 import java.util.Scanner;
 
 public class Skuddaar {
 
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
 
 		System.out.print("År:");
-		int aar = parseInt(input.nextLine());
+		int aar = input.nextInt();
 
 		System.out.print("Måned:");
-		int mnd = parseInt(input.nextLine());
-	
+		int mnd = input.nextInt();
+
 		int antDager = 31;
 
 		if (mnd == 4 || mnd == 6 || mnd == 9 || mnd == 11) {
 			antDager = 30;
 		} else {
-			
+
 			if (mnd == 2) {
 				antDager = 28;
 
 				boolean skuddaar = (aar % 4 == 0 && !((aar % 100 == 0) && !(aar % 400 == 0)));
-				//boolean skuddaar = (aar % 4 == 0 && ((aar % 100 != 0) || (aar % 400 == 0)));
-				
-				if (skuddaar) 
+				// boolean skuddaar = (aar % 4 == 0 && ((aar % 100 != 0) || (aar % 400 == 0)));
+
+				if (skuddaar)
 					antDager = 29;
-				
+
 			}
 		}
 
-		System.out.println(mnd + "-"+aar + ":" + antDager);
+		System.out.println(mnd + "-" + aar + ":" + antDager);
 		input.close();
 	}
 
