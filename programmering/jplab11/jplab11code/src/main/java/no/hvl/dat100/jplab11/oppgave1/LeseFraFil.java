cbd2757af21a03aef5886654fe421952e1e1274c
Lars Michael Kristensen <lmkr@users.noreply.github.com>
2025-10-15 11:35:45 +0200

    jplab 11

---
+++ programmering/jplab11/jplab11code/src/main/java/no/hvl/dat100/jplab11/oppgave1/LeseFraFil.java
@@ -0,0 +1,38 @@
+package no.hvl.dat100.jplab11.oppgave1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class LeseFraFil {
+
+	static String MAPPE_STR = System.getProperty("user.dir") + "/src/main/java/no/hvl/dat100/jplab11/oppgave1/";
+
+	static public void main(String[] args) throws FileNotFoundException {
+
+		String filnavn;
+
+		filnavn = JOptionPane.showInputDialog("Filnavn i mappen " + MAPPE_STR);
+
+		File file = new File(MAPPE_STR + filnavn);
+		Scanner reader = new Scanner(file);
+
+		int linenumber = 1;
+
+		// les innhold i filen linje for linje
+		String line;
+
+		while (reader.hasNextLine()) {
+			line = reader.nextLine();
+			System.out.println(linenumber + " " + line);
+			linenumber++;
+		}
+
+		reader.close();
+
+		// JOptionPane.showMessageDialog(null, "Filen " + filnavn + " finnes
+		// ikke. \n" + e.getMessage());
+	}
+}
